f4a7c15fef9a9758feed46c8c2a1018b99fa9aac
JonathanC3po <Jonathan.conte1938@gmail.com>
2025-11-06 16:12:01 +0000

    finished methods

--- MyLLTester.java
+++ MyLLTester.java
@@ -2,7 +2,8 @@ public class MyLLTester.java
 {
   public static void main(String [] args)
   {
-    
+
+      
 
   } // end main
   
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -1,5 +1,50 @@
 public class MyLinkedList
 {
+    private ListNode head;
+    private int size;
+
+    public MyLinkedList(){
+        head = null;
+        size = 0;
+    }
+
+    public int size(){
+        return size;
+    }
+
+    public boolean isEmpty(){
+        if(size = 0){
+            Return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public boolean addLast(Object obj){
+        ListNode newItem = new ListNode(obj);
+        ListNode current = head;
+        if(head == null){
+            head = newNode;
+            return true;
+        }
+        
+        else{
+            while(getNext(); != null){
+                current = current.getNext();
+            }
+
+        }
+        current.getNext(); = newItem;
+        size++;
+        return true;
+          
+        
+    }
+    public boolean add(Object obj){
+        addLast(Object obj);
+    }
+    
 
 
 }
