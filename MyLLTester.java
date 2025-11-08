dab886dd1f8d8f56e98b4dc59c73bcf2c6cc5c55
JonathanC3po <Jonathan.conte1938@gmail.com>
2025-11-08 06:19:58 +0000

    submit

--- MyLLTester.java
+++ MyLLTester.java
@@ -1,10 +1,50 @@
-public class MyLLTester.java
+public class MyLLTester
 {
+  public static void printList(MyLinkedList list)
+  {
+    System.out.println("Current List:");
+    for (int i = 0; i < list.size(); i++)
+    {
+        System.out.println(i + ": " + list.get(i));
+    }
+    System.out.println();
+  }
+  
   public static void main(String [] args)
   {
+    MyLinkedList list = new MyLinkedList();
 
-      
+    System.out.println("initial list empty:" + list.isEmpty());
+    System.out.println("initial size:" + list.size());
+    System.out.println("adding 10 items:");
 
+    list.addFirst("A");   
+    list.addLast("B");    
+    list.add("C");        
+    list.addFirst("D");   
+    list.addLast("E");    
+    list.add("F");        
+    list.addLast("G");    
+    list.addFirst("H");   
+    list.add("I");        
+    list.addLast("J");    
+
+    System.out.println("list size:" + list.size());
+    System.out.println("list empty?" + list.isEmpty());
+    System.out.println("item at index 5: " + list.get(5));
+    System.out.println("replacing index 3 with X");
+    printList(list);
+    list.set(3, "X");
+    printList(list);
+    System.out.println("Removing first:" + list.removeFirst());
+    printList(list);
+    System.out.println("Removing last:" + list.removeLast());
+    printList(list);
+    System.out.println("Removing index 2:" + list.remove(2));
+    printList(list);
+
+    System.out.println("final list size:" + list.size());
+    System.out.println("is list empty:" + list.isEmpty());
   } // end main
   
 } // end class
--- MyLinkedList.java
+++ MyLinkedList.java
@@ -13,38 +13,131 @@ public class MyLinkedList
     }
 
     public boolean isEmpty(){
-        if(size = 0){
-            Return true;
+        if(size == 0){
+            return true;
         }
         else{
             return false;
         }
-
     }
-    public boolean addLast(Object obj){
-        ListNode newItem = new ListNode(obj);
+
+    public boolean addLast(Object obj)
+    {
+        ListNode newItem = new ListNode(obj, null);
         ListNode current = head;
         if(head == null){
-            head = newNode;
+            head = newItem;
+            size++;
             return true;
         }
-        
         else{
-            while(getNext(); != null){
+            while(current.getNext() != null){
                 current = current.getNext();
             }
-
         }
-        current.getNext(); = newItem;
+        current.setNext(newItem);
+
+        size++;
+
+        return true;
+    }
+
+    public boolean add(Object obj){
+        return addLast(obj);
+    }
+
+    public boolean addFirst(Object obj){
+        ListNode newItem = new ListNode(obj, null);
+        newItem.setNext(head);
+        head = newItem;
         size++;
         return true;
-          
-        
     }
-    public boolean add(Object obj){
-        addLast(Object obj);
+
+    public Object get(int i){
+        if(i < 0 || i >= size){
+            return null;
+        }
+        ListNode current = head;
+        int index = 0;
+        while(index < i){
+            current = current.getNext();
+            index += 1;
+        }
+        return current.getValue();
     }
-    
+
+    public Object set(int i, Object newValue){
+        if(i < 0 || i >= size){
+            return null;
+        }
+        ListNode current = head;
+        int index = 0;
+        while(index < i){
+            current = current.getNext();
+            index += 1;
+        }
+        Object oldValue = current.getValue();
+        current.setValue(newValue);
+        return oldValue;
+    }
+
+    public Object removeFirst(){
+        if(head == null){
+            return null;
+        }
+        Object value = head.getValue();
+        head = head.getNext();
+        size--;
+        return value;
+    }
+
+    public Object removeLast(){
+        if(head == null){
+            return null;
+        }
+        if(head.getNext() == null){
+            Object value = head.getValue();
+            head = null;
+            size--;
+            return value;
+        }
+        ListNode current = head;
+        while(current.getNext().getNext() != null){
+            current = current.getNext();
+        }
+        Object value = current.getNext().getValue();
+        current.setNext(null);
+        size--;
+        return value;
+    }
+
+    public Object remove(int index){
+        if(head == null){
+            return null;
+        }
+        if(index < 0 || index >= size){
+            return null;
 
 
+        }
+        if (index == 0){
+            return removeFirst();
+
+        }
+        ListNode current = head;
+        for (int i = 0; i < index - 1; i++){
+            current = current.getNext();
+        }
+
+
+        ListNode removedNode = current.getNext();
+        Object value = removedNode.getValue();
+        current.setNext(removedNode.getNext());
+        size--;
+        return value;
+
+
+    }
 }
+
